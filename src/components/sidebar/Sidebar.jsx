e7665325d475011741d052006bc8380e6ba5b4f4
Sourabh <sourabhpatole@outlook.com>
2022-10-17 19:02:04 +0530

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,78 @@
+import "./sidebar.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Admin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li>
+            <DashboardIcon />
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <PersonIcon />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreIcon />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <BarChartIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon />
+            <span>Notification</span>
+          </li>
+          <li>
+            <SettingsSystemDaydreamIcon />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon />
+            <span>Setting</span>
+          </li>
+          <li>
+            <AccountCircleIcon />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">color option</div>
+    </div>
+  );
+};
+
+export default Sidebar;
